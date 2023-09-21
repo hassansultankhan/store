ff2c03558c2d3aed1cc8829d68f40737345c2ce8
Hassan workstaion <hassansultankhan@gmail.com>
2023-09-21 16:28:44 +0500

    start before stash

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:estore/navigationScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+           title: 'eStore',
+           theme: ThemeData(
+            primarySwatch: Colors.amber,
+           ),
+           home: navigationScreen(),
+    );
+  }
+}
+  
---
+++ lib/mainScreen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class mainScreen extends StatefulWidget {
+  const mainScreen({super.key});
+
+  @override
+  State<mainScreen> createState() => _mainScreenState();
+}
+
+class _mainScreenState extends State<mainScreen> {
+  String mainScreenTitle ="eStore";
+  List<String> imageAssetPaths = [
+    'assets/images/image1.jpeg',
+    'assets/images/image2.jpeg',
+  ];
+  @override
+  
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+               title: Text(mainScreenTitle),
+               centerTitle: true,
+               toolbarHeight: 60,
+               backgroundColor: Color.fromARGB(255, 204, 235, 26),
+
+      ),
+      
+      body:
+      // Align(
+      //   alignment: Alignment.topCenter,
+      //container with size of screen
+      // child:
+       Container (
+                //  color: Colors.greenAccent,
+                 height: MediaQuery.of(context).size.height,
+                 width :MediaQuery.of(context).size.width,      
+                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children:[
+                  SizedBox(
+                    height: 50,),
+                  //create pictoral slider
+                  // Expanded(
+                  //   child: 
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        height: 200,
+                        enlargeCenterPage: true,
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        enableInfiniteScroll: true,
+                        autoPlayAnimationDuration: Duration(milliseconds:800),
+                        viewportFraction: 0.8,
+                      ),
+                      
+                      items: imageAssetPaths.map((String imageAssetPath) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                              ),
+                              child: InkWell(
+                                onTap: () => loadProduct(),
+                              child:Image.asset(
+                                imageAssetPath,
+                                fit: BoxFit.cover,
+                              ),
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+
+                      )
+
+                
+                 ],
+      
+    ),
+    ),
+   
+    // ),
+    );
+
+  }
+  loadProduct(){}
+}

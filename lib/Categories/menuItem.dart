6e6eb0514b360cf7a06850e0522e396c41efd33d
Hassan Sultan Khan <hassansultankhan.com>
2023-11-13 02:46:11 +0500

    menuitem start

---
+++ lib/Categories/menuItem.dart
@@ -0,0 +1,15 @@
+class Product {
+  late String title;
+  late String category;
+  late String size;
+  late double price;
+  late String imagepath;
+
+  Product({
+    required this.title,
+    required this.category,
+    required this.size,
+    required this.price,
+    required this.imagepath,
+  });
+}

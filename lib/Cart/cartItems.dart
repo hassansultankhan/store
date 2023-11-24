72a74a350ef29864a2ebc08972865727b730b102
Hassan workstaion <hassansultankhan@gmail.com>
2023-11-25 01:04:19 +0500

    25/11/2023

---
+++ lib/Cart/cartItems.dart
@@ -0,0 +1,17 @@
+class CartItem {
+  final String title;
+  final String category;
+  final String size;
+  final int price;
+  final int qtySold;
+  final int productNo;
+
+  CartItem({
+    required this.title,
+    required this.category,
+    required this.size,
+    required this.price,
+    required this.qtySold,
+    required this.productNo,
+  });
+}

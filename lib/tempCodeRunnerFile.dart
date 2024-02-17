32c39b5da81da2fd1ed260d43705fdec7e9e4dba
Hassan workstaion <hassansultankhan@gmail.com>
2024-02-17 12:44:03 +0500

    17/2/24, office

---
+++ lib/tempCodeRunnerFile.dart
@@ -0,0 +1,20 @@
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'eStore',
+      home: loginScreen(),
+    );
+  }
+}

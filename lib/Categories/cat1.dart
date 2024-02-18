3dbd4a2d42bdc49c5795cc45b06d275100faa102
Hassan Sultan Khan <hassansultankhan.com>
2024-02-18 17:11:06 +0500

    18/02/24, home

--- lib/Categories/cat1.dart
+++ lib/Categories/cat1.dart
@@ -1,28 +1,366 @@
+import 'package:estore/Cart/cartItems.dart';
+import 'menuItem.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:estore/Database/dbinitialization.dart';
+// ignore: duplicate_import
+import 'package:estore/Cart/cartItems.dart';
 
 class Cat1 extends StatefulWidget {
   const Cat1({Key? key}) : super(key: key);
 
   @override
-  State<Cat1> createState() => _Cat1State();
+  State<Cat1> createState() => _Cat0State();
 }
 
-class _Cat1State extends State<Cat1> {
+class _Cat0State extends State<Cat1> {
+  List<MenuItem> menuItems = [
+    MenuItem(
+        title: "Black Bean Sauce",
+        category: "Sauces",
+        size: "700ml",
+        price: 650,
+        imagePath: 'assets/images/sauces/BlackBeanSauce.jpg',
+        soldStatus: false,
+        qtySold: 1,
+        productNo: 007),
+    MenuItem(
+        title: "Hot Pepper Sauce",
+        category: "Sauces",
+        size: "500 ml",
+        price: 340,
+        imagePath: 'assets/images/sauces/HotPepperSauce.jpg',
+        soldStatus: false,
+        qtySold: 1,
+        productNo: 008),
+    MenuItem(
+        title: "Keto Sauce",
+        category: "Sauces",
+        size: "700ml",
+        price: 500,
+        imagePath: 'assets/images/sauces/ketoSauce.jpg',
+        soldStatus: false,
+        qtySold: 1,
+        productNo: 009),
+    MenuItem(
+        title: "Mirchi Sauce",
+        category: "Sauces",
+        size: "500ml",
+        price: 250,
+        imagePath: 'assets/images/sauces/mirchisauce.jpg',
+        soldStatus: false,
+        qtySold: 1,
+        productNo: 010),
+    MenuItem(
+        title: "Peanut Sauce",
+        category: "Sauces",
+        size: "700ml",
+        price: 500,
+        imagePath: 'assets/images/sauces/Peanutsauce.jpg',
+        soldStatus: false,
+        qtySold: 1,
+        productNo: 011),
+    MenuItem(
+        title: "Tomato Sauce",
+        category: "Sauces",
+        size: "700ml",
+        price: 500,
+        imagePath: 'assets/images/sauces/Tomatosauce.jpg',
+        soldStatus: false,
+        qtySold: 1,
+        productNo: 012),
+  ];
+
+  List<CartItem> cartItems = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Cat 1'),
-      //   backgroundColor: Color.fromARGB(255, 204, 235, 26),
-      // ),
+      body: ListView.builder(
+        itemCount: menuItems.length,
+        itemBuilder: ((context, index) {
+          MenuItem menuItem = menuItems[index];
 
-      // body: ListView.builder(
-      //   itemCount: ,
-      //   itemBuilder: (BuildContext c, int index){
+          return Container(
+            child: Column(children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+                child: ListTile(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    tileColor: const Color.fromARGB(255, 189, 223, 207),
+                    leading: CircleAvatar(
+                        backgroundImage: AssetImage(menuItem.imagePath),
+                        radius: 30,
+                        backgroundColor: Colors.grey),
+                    title: Text(menuItem.title,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    subtitle: Text("${menuItem.price.toString()} Rs.",
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w600)),
+                    trailing: Icon(
+                      Icons.add_shopping_cart_rounded,
+                      size: 30,
+                      color: menuItem.soldStatus
+                          ? Colors.green
+                          : const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    onTap: () {
+                      Future.delayed(Duration.zero, () {
+                        showDialog(
+                          context: context, //create location tag
+                          builder: (context) => itemAlert(
+                              context, //pass location tag to itemAlert, that where does itemAlert lyes
+                              menuItem.imagePath,
+                              menuItem.title,
+                              menuItem.category,
+                              menuItem.size,
+                              menuItem.price,
+                              menuItem.qtySold,
+                              menuItem.productNo,
+                              menuItem.soldStatus, () {
+                            // Logic to update soldStatus to true
+                            setState(() {
+                              //function passed as parameter to itemAlert
+                              menuItem.soldStatus = !menuItem.soldStatus;
+                            });
+                          }),
+                        );
+                      });
+                    }),
+              ),
+            ]),
+          );
+        }),
+      ),
+    );
+  }
 
-      //   }
+  //Alert box for item details
 
-      //   ),
+  Widget itemAlert(
+    BuildContext context,
+    String _imagePath,
+    String _title,
+    String _category,
+    String _size,
+    int _price,
+    int _qtySold,
+    int _productNo,
+    bool _soldStatus,
+    Function() updateSoldStatus,
+  ) {
+    int quantity = 1;
+
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter alertSetState) {
+        return AlertDialog(
+          contentPadding: const EdgeInsets.all(5),
+          elevation: 4,
+          title: Text(
+            _title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(color: Colors.green),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      clipBehavior: Clip.antiAlias,
+                      child: Image.asset(
+                        _imagePath,
+                        height: 150,
+                        width: 150,
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Category: $_category"),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text("Size: $_size"),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text("Price: $_price"),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, top: 20),
+                          child: IconButton(
+                            onPressed: () async {
+                              //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                              // update cart
+                              updateSoldStatus();
+                              alertSetState(() {
+                                _soldStatus = !_soldStatus;
+                              });
+                              showToast(_soldStatus);
+
+                              print('Title in itemAlert: $_title');
+
+                              // Create a CartItem object
+                              CartItem cartItem = CartItem.withoutId(
+                                // id: remove id assignment as it will be autogenerated
+                                title: _title,
+                                category: _category,
+                                size: _size,
+                                price: _price,
+                                imagePath: _imagePath,
+                                qtySold: _qtySold,
+                                productNo: _productNo,
+                              );
+
+                              // Get the database instance
+                              Dbfiles dbfiles = Dbfiles();
+                              await dbfiles
+                                  .db; // Ensure the database is initialized
+
+                              if (_soldStatus) {
+                                // Insert the CartItem into the database
+                                await dbfiles.insertCartItem(cartItem);
+                                // After inserting into the database
+                                // Print the image path to make sure the url value is passed
+                                print(
+                                    'Inserted Cart Item Image Path: ${cartItem.imagePath}');
+                              } else {
+                                // If _soldStatus is false, delete the entry with the given id
+                                //   await dbfiles.deleteCartItem(cartItem.id);
+                                await dbfiles
+                                    .deleteCartItem(cartItem.productNo);
+
+                                print(
+                                    "item deleted on _soldStatus being false");
+                              }
+
+                              // Retrieve and print all cart items from the database
+                              List<CartItem> allCartItems =
+                                  await dbfiles.getCartItems();
+                              for (CartItem cartItem in allCartItems) {
+                                print('Cart Item ID: ${cartItem.id}\n'
+                                    'Title: ${cartItem.title}\n'
+                                    'Category: ${cartItem.category}\n'
+                                    'Size: ${cartItem.size}\n'
+                                    'Price: ${cartItem.price}\n'
+                                    'Image URL: ${cartItem.imagePath}\n'
+                                    'Quantity Sold: ${cartItem.qtySold}\n'
+                                    'Product No. : ${cartItem.productNo}\n');
+                              }
+                              setState(() {});
+                            },
+                            icon: Icon(
+                              Icons.add_shopping_cart_rounded,
+                              color: _soldStatus ? Colors.green : Colors.black,
+                              size: 40,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 27, top: 10),
+                          child: Text(
+                            "Place Order",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        if (quantity > 1) {
+                          alertSetState(() {
+                            quantity--;
+                            _qtySold = quantity;
+                            print(_qtySold.toString());
+                          });
+                        }
+                      },
+                      icon: Icon(Icons.remove_circle),
+                    ),
+                    SizedBox(
+                      width: 50,
+                      child: Center(
+                        child: Text(
+                          '$quantity',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        alertSetState(() {
+                          quantity++;
+                          _qtySold = quantity;
+                          print(_qtySold.toString());
+                        });
+                      },
+                      icon: Icon(Icons.add_box),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.close,
+                size: 30,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showToast(bool SoldMessageStatus) async {
+    String alertMessage;
+    if (SoldMessageStatus) {
+      alertMessage = "Order added to Cart";
+    } else {
+      alertMessage = "Product removed from Cart";
+    }
+    await Fluttertoast.showToast(
+      msg: alertMessage,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.grey,
+      textColor: Colors.white,
+      fontSize: 16.0,
     );
   }
 }
--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 class mainScreen extends StatefulWidget {
-  const mainScreen({super.key});
+  mainScreen({super.key});
 
   @override
   State<mainScreen> createState() => _mainScreenState();
@@ -17,54 +17,81 @@ class _mainScreenState extends State<mainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body:Container(
-  
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            //create pictoral slider
-            // Expanded(
-            //   child:
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 200,
-                enlargeCenterPage: true,
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enableInfiniteScroll: true,
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                viewportFraction: 0.8,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
               ),
-              items: imageAssetPaths.map((String imageAssetPath) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                      ),
-                      child: InkWell(
-                        onTap: () => loadProduct(),
-                        child: Image.asset(
-                          imageAssetPath,
-                          fit: BoxFit.cover,
+              //create pictoral slider
+              // Expanded(
+              //   child:
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 200,
+                  enlargeCenterPage: true,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 0.8,
+                ),
+                items: imageAssetPaths.map((String imageAssetPath) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: const BoxDecoration(
+                          color: Colors.grey,
                         ),
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-            )
-          ],
+                        child: InkWell(
+                          onTap: () => loadProduct(),
+                          child: Image.asset(
+                            imageAssetPath,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 100,
+                      color: Colors.lightGreen,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.lightGreen,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.lightGreen,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.lightGreen,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.lightGreen,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-
       // ),
     );
   }

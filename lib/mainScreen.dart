d0857efcffc81d137fd012b2afe5da858780f7b9
Hassan Sultan Khan <hassansultankhan.com>
2024-03-27 01:03:01 +0500

    27/03/24

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class mainScreen extends StatefulWidget {
@@ -23,7 +24,11 @@ class _mainScreenState extends State<mainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-       
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover),
+        ),
         width: MediaQuery.of(context).size.width,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -148,65 +153,70 @@ class _mainScreenState extends State<mainScreen> {
                     Padding(
                       padding: const EdgeInsets.only(top: 10),
                       child: UnconstrainedBox(
-                          child: Container(
+                        child: Container(
                           height: 110,
-                          width: MediaQuery.of(context).size.width-20,
-                          decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 172, 211, 242),
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(20),
+                          width: MediaQuery.of(context).size.width - 50,
+                          decoration: const BoxDecoration(
+                            color: Color.fromARGB(255, 172, 211, 242),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+
+                            //
                           ),
-                        
-                          // 
-                        ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  SizedBox(width: 30),
-                                  Container(
-                                    width: 100,
-                                    height: 100,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(
-                                          10), // Adjust the radius as needed
-                                    ),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(
-                                          5), // Same radius as the BoxDecoration
-                                      child: Image.asset(
-                                        'assets/images/bottles.png',
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              SizedBox(width: 30),
+                              Container(
+                                width: 100,
+                                height: 100,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(
+                                      10), // Adjust the radius as needed
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(
+                                      5), // Same radius as the BoxDecoration
+                                  child: Image.asset(
+                                    'assets/images/bottles.png',
+                                    fit: BoxFit.cover,
                                   ),
-                                  SizedBox(width: 10),
-                                  Container(
-                                    child: RichText(
-                                      text: const TextSpan(
-                                        style: TextStyle(
-                                          fontFamily: 'Poppins regular',
-                                          fontWeight: FontWeight.w600,
-                                          color: Colors.black, // Default color
-                                        ),
-                                        children: [
-                                          TextSpan(
-                                            text: "SPICE up your taste buds\n",
-                                          ),
-                                          TextSpan(
-                                            text: "with HOT SAUCE",
-                                            style: TextStyle(
-                                                color:
-                                                    Color.fromARGB(255, 221, 114, 20),
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
+                                ),
                               ),
+                              SizedBox(width: 10),
+                              Container(
+                                child: RichText(
+                                  text: const TextSpan(
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins regular',
+                                      color: Colors.black, // Default color
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: "SPICE up your taste buds\n",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight
+                                              .w600, // Apply font weight here
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: "with HOT SAUCE",
+                                        style: TextStyle(
+                                          color:
+                                              Color.fromARGB(255, 221, 114, 20),
+                                          fontWeight: FontWeight
+                                              .bold, // Apply font weight here
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
                     ),
 
                     // Element 3 of scroll list
@@ -216,55 +226,52 @@ class _mainScreenState extends State<mainScreen> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          // Container(
-                          //   child: RichText(
-                          //     text: const TextSpan(
-                          //       style: TextStyle(
-                          //         fontFamily: 'Poppins regular',
-                          //         fontWeight: FontWeight.w600,
-                          //         color: Colors.black, // Default color
-                          //       ),
-                          //       children: [
-                          //         TextSpan(
-                          //           text: "Ketchup made from\n*rich ",
-                          //         ),
-                          //         TextSpan(
-                          //             text: "ORGANIC ",
-                          //             style: TextStyle(
-                          //                 color:
-                          //                     Color.fromARGB(255, 4, 155, 9))),
-                          //         TextSpan(
-                          //           text: "tomatoes",
-                          //           style: TextStyle(color: Colors.red),
-                          //         ),
-                          //         TextSpan(
-                          //           text: ".\nADD FLAVOR TO ANYTHING!",
-                          //         ),
-                          //       ],
-                          //     ),
-                          //   ),
-                          // ),
-                       
                           Container(
-                            width: MediaQuery.of(context).size.width-20,
-                            height: 120,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                  10), // Adjust the radius as needed
+                            child: RichText(
+                              text: const TextSpan(
+                                style: TextStyle(
+                                  fontFamily: 'Poppins regular',
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black, // Default color
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: "Confused?\n",
+                                  ),
+                                  TextSpan(text: "Try "),
+                                  TextSpan(
+                                    text: "VARIETY MIX ",
+                                    style: TextStyle(color: Colors.blue),
+                                  ),
+                                ],
+                              ),
                             ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(
-                                  5), // Same radius as the BoxDecoration
-                              child: Image.asset(
-                                'assets/images/bottleVariety.png',
-                                fit: BoxFit.cover,
+                          ),
+                          const SizedBox(width: 15),
+                          Material(
+                            color: Color.fromARGB(255, 221, 114, 20),
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            elevation: 8,
+                            child: Container(
+                              padding: EdgeInsets.all(10),
+                              child: Container(
+                                height: 100,
+                                width: 195,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  image: const DecorationImage(
+                                    image: AssetImage(
+                                      'assets/images/bottleVariety.png',
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
                         ],
                       ),
                     ),
-
+                    SizedBox(height: 20)
                   ],
                 ),
               ),
